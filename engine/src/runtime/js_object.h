80de0e3583ba41147e708ca551dc91da24fe82eb
Tyler Williams <tyler@coolsoftware.dev>
2026-06-21 21:46:11 -0400

    feat: add runtime representation of objects

---
+++ engine/src/runtime/js_object.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "common/value.h"
+#include "runtime/heap_object.h"
+
+#include <unordered_map>
+#include <vector>
+
+namespace yatsi {
+
+class JsObject : public HeapObject {
+public:
+  JsObject();
+
+  void set(const std::string &key, Value val);
+  bool get(const std::string &key, Value &out) const;
+  bool has(const std::string &key) const;
+  void trace(GarbageCollector &gc) override;
+
+private:
+  std::vector<std::pair<std::string, Value>> properties_;
+  std::unordered_map<std::string, size_t> property_index_;
+};
+
+} // namespace yatsi
